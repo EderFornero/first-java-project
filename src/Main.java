db803d882ab6148bc46ce342857b53864ccbcf6f
Eder <ederfornero.ef@gmail.com>
2023-11-23 17:39:35 -0300

    int to byte

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
 
     public static void main (String[] args){
-        System.out.print("Hello world");
+        //System.out.print("Hello world");
+
+        String myString = "My string";
+        // System.out.print(myString);
+
+        Integer myInt = 10;
+        // System.out.print(myInt);
+        myInt = 7;
+        System.out.print(myInt + "Integer value represented" + " ");
+        myInt.byteValue();
+        System.out.print(myInt + "Byte value represented");
     }
 }
